51bfcbe1b65bfc939510fbd0ce4f8adfc0b03c7d
Birch Browning <b.browning@csuohio.edu>
2015-03-19 21:30:17 -0400

    first commit

---
+++ TipCalculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TipCalculator
+//
+//  Created by Birch Browning on 3/19/15.
+//  Copyright (c) 2015 Birch Browning. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TipCalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TipCalculator
+//
+//  Created by Birch Browning on 3/19/15.
+//  Copyright (c) 2015 Birch Browning. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
